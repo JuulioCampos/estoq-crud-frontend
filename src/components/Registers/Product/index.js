a09af1a31db1405b8ded919438052a74976622de
fauzdevbr <juliofcneto2@gmail.com>
2023-07-11 18:53:45 -0300

    ordenacao nas tables e melhorias visuais

--- src/components/Registers/Product/index.js
+++ src/components/Registers/Product/index.js
@@ -1,5 +1,4 @@
-/* eslint-disable react-hooks/exhaustive-deps */
-import { useContext, useEffect } from "react";
+import { useContext, useState } from "react";
 import { Container, Form, Button, Table, Col, Row } from "react-bootstrap";
 import Swal from "sweetalert2";
 import { ProductContext } from "../../../providers/Product";
@@ -8,57 +7,87 @@ import { ProductTypeContext } from "../../../providers/ProductType";
 export const Product = (props) => {
     const { product, setProduct } = useContext(ProductContext);
     const { productType, setProductType } = useContext(ProductTypeContext);
+    const [searchText, setSearchText] = useState("");
+    const [sortConfig, setSortConfig] = useState({ column: null, direction: "asc" });
 
     const confirmButton = (item) => {
         Swal.fire({
-            title: 'Are you sure?',
+            title: "Are you sure?",
             text: "You won't be able to revert this!",
-            icon: 'warning',
+            icon: "warning",
             showCancelButton: true,
-            confirmButtonColor: '#3085d6',
-            cancelButtonColor: '#d33',
-            confirmButtonText: 'Yes, delete it!'
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!",
         }).then((result) => {
             if (result.isConfirmed) {
-                Swal.fire(
-                    `${item.description} was deleted`,
-                    'With Success!',
-                    'success'
-                )
+                Swal.fire(`${item.description} was deleted`, "With Success!", "success");
             }
-        })
-    }
+        });
+    };
+
     const editButton = async (item) => {
         const { value: formValues } = await Swal.fire({
-            title: 'Change Product Type',
+            title: "Change Product Type",
             html:
                 `<input style="margin: 10px;" id="swal-input1 description-edit" type="text" placeholder="${item.product}" class="swal2-input">` +
                 `<input style="margin: 10px;" id="swal-input2 description-edit" type="number" value="${item.price}" class="swal2-input">` +
                 `<select style="width: 88%;height: 2.625em;padding: 10px 0; border-radius: 4px; border: 2px solid lightblue;" id="swal-input3"  class="swal2-input">` +
 
+                productType &&
                 productType.map((product, index) => {
                     return (
-                        `<option value="${product.id}" key="${index}" ${product.id === item.product_type_id ? "selected" : ""}>${product.description}</option>`
-                    )
+                        `<option value="${product.id}" key="${index}" ${product.id === item.product_type_id ? "selected" : ""
+                        }>${product.description}</option>`
+                    );
                 }) +
-                `</select>`
-            ,
+                `</select>`,
             focusConfirm: false,
-            confirmButtonText: 'Yes, change it!',
+            confirmButtonText: "Yes, change it!",
             showCancelButton: true,
 
             preConfirm: () => {
                 return [
-                    document.getElementById('swal-input1').value,
-                    document.getElementById('swal-input2').value
-                ]
-            }
-        })
+                    document.getElementById("swal-input1").value,
+                    document.getElementById("swal-input2").value,
+                ];
+            },
+        });
 
         if (formValues) {
-            Swal.fire(JSON.stringify(formValues))
+            Swal.fire(JSON.stringify(formValues));
         }
-    }
+    };
+
+    const handleSearchChange = (e) => {
+        const value = e.target.value || "";
+        setSearchText(value);
+    };
+
+    const handleSort = (column) => {
+        setSortConfig((prevSortConfig) => {
+            if (prevSortConfig.column === column && prevSortConfig.direction === "asc") {
+                return { column, direction: "desc" };
+            }
+            return { column, direction: "asc" };
+        });
+    };
+
+    const sortedProduct = product
+        ? [...product].sort((a, b) => {
+            if (sortConfig.column) {
+                const keyA = a[sortConfig.column];
+                const keyB = b[sortConfig.column];
+                if (keyA < keyB) return sortConfig.direction === "asc" ? -1 : 1;
+                if (keyA > keyB) return sortConfig.direction === "asc" ? 1 : -1;
+            }
+            return 0;
+        })
+        : [];
+
+    const filteredProduct = sortedProduct.filter((item) =>
+        item.product.toLowerCase().includes(searchText.toLowerCase())
+    );
 
     return (
         <>
@@ -75,11 +104,14 @@ export const Product = (props) => {
                             <Form.Label>Product Type</Form.Label>
                             <Form.Select aria-label="Default select example">
                                 <option hidden>Select type</option>
-                                {productType.map((item, index) => {
-                                    return (
-                                        <option value={item.id} key={item.id}>{item.description}</option>
-                                    )
-                                })}
+                                {productType &&
+                                    productType.map((item, index) => {
+                                        return (
+                                            <option value={item.id} key={item.id}>
+                                                {item.description}
+                                            </option>
+                                        );
+                                    })}
                                 <option value="2">Two</option>
                             </Form.Select>
                         </Col>
@@ -91,38 +123,67 @@ export const Product = (props) => {
             <h2 className="text-center">Your Product List</h2>
             <div className="table-product-type-list">
                 <Container>
+                    <Form>
+                        <Form.Group className="mb-3" controlId="formSearch">
+                            <Form.Control
+                                type="text"
+                                placeholder="Search by product"
+                                value={searchText}
+                                onChange={handleSearchChange}
+                            />
+                        </Form.Group>
+                    </Form>
                     <Table striped bordered hover>
                         <thead>
                             <tr>
-                                <th>Id.</th>
-                                <th>Product</th>
-                                <th>Price</th>
-                                <th>Type</th>
-                                <th>Action</th>
-
+                                <th>
+                                    <Button className="bold" variant="black" onClick={() => handleSort("id")}>
+                                        Id. ↕
+                                    </Button>
+                                </th>
+                                <th>
+                                    <Button className="bold" variant="black" onClick={() => handleSort("product")}>
+                                        Product ↕
+                                    </Button>
+                                </th>
+                                <th>
+                                    <Button className="bold" variant="black" onClick={() => handleSort("price")}>
+                                        Price ↕
+                                    </Button>
+                                </th>
+                                <th>
+                                    <Button className="bold" variant="black" onClick={() => handleSort("type")}>
+                                        Type ↕
+                                    </Button>
+                                </th>
+                                <th>
+                                    <Button className="bold" variant="black" onClick={() => handleSort("action")}>
+                                        Action
+                                    </Button>
+                                </th>
                             </tr>
                         </thead>
                         <tbody>
-                            {
-                                product && product.map((item, index) => {
-                                    return (
-                                        <tr key={index}>
-                                            <td>{item.id}</td>
-                                            <td>{item.product}</td>
-                                            <td>${item.price}</td>
-                                            <td>{item.description}</td>
-                                            <td>
-                                                <Button className="m-1" variant="warning" onClick={() => editButton(item)}>Edit</Button>
-                                                <Button className="m-1" variant="danger" onClick={() => confirmButton(item)}>Delete</Button>
-                                            </td>
-                                        </tr>
-                                    )
-                                })
-                            }
+                            {filteredProduct.map((item, index) => (
+                                <tr key={index}>
+                                    <td>{item.id}</td>
+                                    <td>{item.product}</td>
+                                    <td>${item.price}</td>
+                                    <td>{item.description}</td>
+                                    <td>
+                                        <Button className="m-1" variant="warning" onClick={() => editButton(item)}>
+                                            Edit
+                                        </Button>
+                                        <Button className="m-1" variant="danger" onClick={() => confirmButton(item)}>
+                                            Delete
+                                        </Button>
+                                    </td>
+                                </tr>
+                            ))}
                         </tbody>
                     </Table>
                 </Container>
-            </div>
+            </div >
         </>
     );
-}
+};
--- src/components/Registers/ProductType/index.js
+++ src/components/Registers/ProductType/index.js
@@ -1,49 +1,79 @@
-import { useContext } from "react";
-import { Container, Form, Button, Table, Row, Col } from "react-bootstrap";
+import { useContext, useState } from "react";
+import { Container, Form, Button, Table, Col, Row } from "react-bootstrap";
 import Swal from "sweetalert2";
 import { ProductTypeContext } from "../../../providers/ProductType";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 
 export const ProductType = (props) => {
     const { productType, setProductType } = useContext(ProductTypeContext);
+    const [searchText, setSearchText] = useState("");
+    const [sortConfig, setSortConfig] = useState({ column: null, direction: "asc" });
 
     const confirmButton = (item) => {
         Swal.fire({
-            title: 'Are you sure?',
+            title: "Are you sure?",
             text: "You won't be able to revert this!",
-            icon: 'warning',
+            icon: "warning",
             showCancelButton: true,
-            confirmButtonColor: '#3085d6',
-            cancelButtonColor: '#d33',
-            confirmButtonText: 'Yes, delete it!'
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!",
         }).then((result) => {
             if (result.isConfirmed) {
-                Swal.fire(
-                    `${item.description} was deleted`,
-                    'With Success!',
-                    'success'
-                )
+                Swal.fire(`${item.description} was deleted`, "With Success!", "success");
             }
-        })
-    }
+        });
+    };
+
     const editButton = async (item) => {
         const { value: formValues } = await Swal.fire({
-            title: 'Change Product Type',
+            title: "Change Product Type",
             html:
-              `<input id="swal-input1 description-edit" type="text" placeholder="${item.description}" class="swal2-input">` +
-              `<input id="swal-input2 tax-edit" type="number" value="${item.tax}" class="swal2-input">`,
+                `<input id="swal-input1 description-edit" type="text" placeholder="${item.description}" class="swal2-input">` +
+                `<input id="swal-input2 tax-edit" type="number" value="${item.tax}" class="swal2-input">`,
             focusConfirm: false,
             preConfirm: () => {
-              return [
-                document.getElementById('swal-input1').value,
-                document.getElementById('swal-input2').value
-              ]
+                return [
+                    document.getElementById("swal-input1").value,
+                    document.getElementById("swal-input2").value,
+                ];
+            },
+        });
+
+        if (formValues) {
+            Swal.fire(JSON.stringify(formValues));
+        }
+    };
+
+    const handleSearchChange = (e) => {
+        const value = e.target.value || "";
+        setSearchText(value);
+    };
+
+    const handleSort = (column) => {
+        setSortConfig((prevSortConfig) => {
+            if (prevSortConfig.column === column && prevSortConfig.direction === "asc") {
+                return { column, direction: "desc" };
             }
-          })
-          
-          if (formValues) {
-            Swal.fire(JSON.stringify(formValues))
-          }
-    }
+            return { column, direction: "asc" };
+        });
+    };
+
+    const sortedProductType = productType
+        ? [...productType].sort((a, b) => {
+            if (sortConfig.column) {
+                const keyA = a[sortConfig.column];
+                const keyB = b[sortConfig.column];
+                if (keyA < keyB) return sortConfig.direction === "asc" ? -1 : 1;
+                if (keyA > keyB) return sortConfig.direction === "asc" ? 1 : -1;
+            }
+            return 0;
+        })
+        : [];
+
+    const filteredProductType = sortedProductType.filter((item) =>
+        item.description.toLowerCase().includes(searchText.toLowerCase())
+    );
 
     return (
         <>
@@ -58,10 +88,7 @@ export const ProductType = (props) => {
                         </Col>
                         <Col md={4}>
                             <Form.Label>Tax</Form.Label>
-                            <Form.Control
-                                type="number"
-                                id="tax"
-                            />
+                            <Form.Control type="number" id="tax" />
                         </Col>
                     </Row>
                     <Button className="mt-2">Create</Button>
@@ -71,36 +98,71 @@ export const ProductType = (props) => {
             <h2 className="text-center">Your Product Types</h2>
             <div className="table-product-type-list">
                 <Container>
+                    <Form>
+                        <Form.Group className="mb-3" controlId="formSearch">
+                            <Form.Control
+                                type="text"
+                                placeholder="Search by description"
+                                value={searchText}
+                                onChange={handleSearchChange}
+                            />
+                        </Form.Group>
+                    </Form>
                     <Table striped bordered hover>
                         <thead>
                             <tr>
-                                <th>Id.</th>
-                                <th>Description</th>
-                                <th>Tax</th>
-                                <th>Action</th>
-
+                                <th>
+                                    <Button className="bold" variant="black" onClick={() => handleSort("id")}>
+                                        id ↕
+                                    </Button>
+                                </th>
+                                <th>
+                                    <Button
+                                        className="bold"
+                                        variant="black"
+                                        onClick={() => handleSort("description")}
+                                    >
+                                        Description ↕
+                                    </Button>
+                                </th>
+                                <th>
+                                    <Button className="bold" variant="black" onClick={() => handleSort("tax")}>
+                                        Tax ↕
+                                    </Button>
+                                </th>
+                                <th>
+                                    <Button className="bold" variant="black" onClick={() => handleSort("Action")}>
+                                        Action
+                                    </Button>
+                                </th>
                             </tr>
                         </thead>
-                        <tbody>
-                            {
-                                productType.map((item, index) => {
-                                    return (
-                                        <tr key={index}>
-                                            <td>{item.id}</td>
-                                            <td>{item.description}</td>
-                                            <td>{item.tax}</td>
-                                            <td>
-                                                <Button className="m-1" variant="warning" onClick={() => editButton(item)}>Edit</Button>
-                                                <Button className="m-1" variant="danger" onClick={() => confirmButton(item)}>Delete</Button>
-                                            </td>
-                                        </tr>
-                                    )
-                                })
-                            }
-                        </tbody>
+                        <TransitionGroup component="tbody">
+                            {filteredProductType.map((item, index) => (
+                                <CSSTransition key={index} classNames="fade" timeout={500}>
+                                    <tr>
+                                        <td>{item.id}</td>
+                                        <td>{item.description}</td>
+                                        <td>{item.tax}</td>
+                                        <td>
+                                            <Button className="m-1" variant="warning" onClick={() => editButton(item)}>
+                                                Edit
+                                            </Button>
+                                            <Button
+                                                className="m-1"
+                                                variant="danger"
+                                                onClick={() => confirmButton(item)}
+                                            >
+                                                Delete
+                                            </Button>
+                                        </td>
+                                    </tr>
+                                </CSSTransition>
+                            ))}
+                        </TransitionGroup>
                     </Table>
                 </Container>
             </div>
         </>
     );
-}
+};
